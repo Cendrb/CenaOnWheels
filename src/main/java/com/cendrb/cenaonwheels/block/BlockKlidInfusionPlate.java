ebdc754177063990ea870692c308dc584dc76b01
Čeněk Drbal <cendrb@gmail.com>
2016-10-19 21:32:23 +0200

    Why am I so bad at creating textures

--- src/main/java/com/cendrb/cenaonwheels/block/BlockKlidInfusionPlate.java
+++ src/main/java/com/cendrb/cenaonwheels/block/BlockKlidInfusionPlate.java
@@ -1,6 +1,7 @@
 package com.cendrb.cenaonwheels.block;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 
 /**
  * Created by cendr_000 on 18.10.2016.
@@ -16,5 +17,15 @@ public class BlockKlidInfusionPlate extends BlockBase {
 
     public float getEfficiency() {
         return efficiency;
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public boolean isFullCube(IBlockState state) {
+        return false;
     }
 }
