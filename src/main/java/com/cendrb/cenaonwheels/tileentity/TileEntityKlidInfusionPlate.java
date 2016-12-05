7b881d5bf92737406e71c284718ba98f477fd7d9
Cendrb <cendrb@gmail.com>
2016-12-05 22:11:40 +0100

    Add some klid infusion logic to TileEntityKlidInfusionPlate

--- src/main/java/com/cendrb/cenaonwheels/tileentity/TileEntityKlidInfusionPlate.java
+++ src/main/java/com/cendrb/cenaonwheels/tileentity/TileEntityKlidInfusionPlate.java
@@ -1,8 +1,14 @@
 package com.cendrb.cenaonwheels.tileentity;
 
 import com.cendrb.cenaonwheels.IKlidAcceptor;
+import com.cendrb.cenaonwheels.init.KlidInfusionRecipe;
+import com.cendrb.cenaonwheels.init.KlidInfusionRecipeResult;
 import com.cendrb.cenaonwheels.init.ModBlocks;
+import com.cendrb.cenaonwheels.init.ModKlidInfusionRecipes;
+import com.cendrb.cenaonwheels.util.COWLogger;
+import com.cendrb.cenaonwheels.util.WorldHelper;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
@@ -15,6 +21,9 @@ import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by cendr_000 on 21.10.2016.
  */
@@ -23,11 +32,15 @@ public class TileEntityKlidInfusionPlate extends TileEntity implements ITickable
     // in case the nbt is corrupt use this default value
     private float efficiency = ModBlocks.klidInfusionBasicPlate.getEfficiency();
     private int klidInfused = 0;
+    private ArrayList<Item> currentIngredients;
+    private boolean infusionRunning;
+    private KlidInfusionRecipe currentRecipe;
 
     private MainItemStackHandler itemStackHandler;
 
     public TileEntityKlidInfusionPlate() {
         itemStackHandler = new MainItemStackHandler();
+        currentIngredients = new ArrayList<>();
     }
 
     @Override
@@ -72,23 +85,69 @@ public class TileEntityKlidInfusionPlate extends TileEntity implements ITickable
 
     @Override
     public void acceptKlid(int amount) {
-
+        if (infusionRunning) {
+            infuseWith(amount);
+        } else {
+            if (isInfusionReady()) {
+                infusionRunning = true;
+                infuseWith(amount);
+            } else {
+                WorldHelper.releaseKlidAt(worldObj, pos.getX(), pos.getY(), pos.getZ(), amount);
+            }
+        }
     }
 
-    public static class MainItemStackHandler extends ItemStackHandler {
+    private void infuseWith(int klidAmount) {
+        int efficientKlidAmount = (int) (klidAmount * efficiency);
+        int klidLost = klidAmount - efficientKlidAmount;
+        WorldHelper.releaseKlidAt(worldObj, pos.getX(), pos.getY(), pos.getZ(), klidLost);
+
+        klidInfused += efficientKlidAmount;
+        if (klidInfused >= currentRecipe.getRequiredKlid()) {
+            infusionRunning = false;
+            currentRecipe = null;
+            currentIngredients = new ArrayList<>();
+            if (klidInfused > currentRecipe.getRequiredKlid())
+                WorldHelper.releaseKlidAt(worldObj, pos.getX(), pos.getY(), pos.getZ(), klidInfused - currentRecipe.getRequiredKlid());
+        }
+    }
+
+    public boolean isInfusionReady() {
+        return currentRecipe != null;
+    }
+
+    public boolean isInfusionRunning()
+    {
+        return infusionRunning;
+    }
+
+    public class MainItemStackHandler extends ItemStackHandler {
         public MainItemStackHandler() {
             setSize(1);
         }
 
         @Override
         public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
-            if (stacks[0] == null || stacks[0].stackSize != 0) {
+            if (infusionRunning || stacks[0] == null || stacks[0].stackSize != 0) {
                 return stack;
             } else {
                 // check if can be klid infused
-
+                ArrayList<Item> theoreticalFutureList = new ArrayList<>(currentIngredients);
+                for (int i = 0; i < stack.stackSize; i++)
+                    theoreticalFutureList.add(stack.getItem());
+                KlidInfusionRecipeResult result = ModKlidInfusionRecipes.isPartOfRecipe(theoreticalFutureList);
+                if (result == KlidInfusionRecipeResult.Complete) {
+                    COWLogger.logDebug("Infusion ready!");
+                    currentRecipe = ModKlidInfusionRecipes.getRecipeFor(theoreticalFutureList);
+                    currentIngredients = theoreticalFutureList;
+                    return null;
+                } else if (result == KlidInfusionRecipeResult.PartOfTheRecipe) {
+                    currentIngredients = theoreticalFutureList;
+                    return null;
+                } else {
+                    return stack;
+                }
             }
-            return null;
         }
 
         @Override
