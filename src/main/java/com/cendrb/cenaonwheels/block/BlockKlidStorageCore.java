1c1a5dd2768e20376c6c15daa8930bbeb06251bd
Cendrb <cendrb@gmail.com>
2016-12-06 22:21:44 +0100

    Add core klid per burst info to tooltips

--- src/main/java/com/cendrb/cenaonwheels/block/BlockKlidStorageCore.java
+++ src/main/java/com/cendrb/cenaonwheels/block/BlockKlidStorageCore.java
@@ -1,6 +1,10 @@
 package com.cendrb.cenaonwheels.block;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+
+import java.util.List;
 
 /**
  * Created by cendr_000 on 02.10.2016.
@@ -16,5 +20,11 @@ public class BlockKlidStorageCore extends BlockKlidStoragePart {
 
     public int getValue() {
         return value;
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, EntityPlayer player, List<String> tooltip, boolean advanced) {
+        tooltip.add("Klid per burst (see JEI): " + value);
+        super.addInformation(stack, player, tooltip, advanced);
     }
 }
